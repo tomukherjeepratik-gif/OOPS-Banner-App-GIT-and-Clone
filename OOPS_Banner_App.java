b4fe3e20396b61731a6e673da1befa78a5f9207b
Pratik Mukherjee <pratikmukherjee@Pratiks-MacBook-Air-6.local>
2026-02-23 03:43:43 +0530

    new fiel added oops banner app file

---
+++ OOPS_Banner_App.java
@@ -0,0 +1,8 @@
+public class OOPS_Banner_App {
+    public static void main(){
+        System.out.println("**** **** **** ****");
+        System.out.println("*  * *  * *  * **  ");
+        System.out.println("*  * *  * *      **");
+        System.out.println("**** **** *    ****");
+    }
+}
