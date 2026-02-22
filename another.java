fa0269284eb500b47d094fdc829c0dbac8ed77f0
Pratik Mukherjee <pratikmukherjee@Pratiks-MacBook-Air-6.local>
2026-02-23 04:15:28 +0530

    new file oops banner app with strong.join used

---
+++ another.java
@@ -0,0 +1,8 @@
+public class another {
+    public static void main(){
+    System.out.println(String.join("","**** **** **** ****"));
+    System.out.println(String.join("","*  * *  * *  * **  "));
+    System.out.println(String.join("","*  * *  * *      ** "));
+    System.out.println(String.join("","**** **** *    ****"));
+    }
+}
