6810807f4849a9c69e05a25c5278004e4caf802b
Pratik Mukherjee <pratikmukherjee@Pratiks-MacBook-Air-6.local>
2026-02-24 09:54:41 +0530

    new file a.java has been added

---
+++ a.java
@@ -0,0 +1,14 @@
+public class a {
+    public static void main(){
+        String[] lines={
+            String.join("","**** **** **** ****"),
+            String.join("","*  * *  * *  * **  "),
+            String.join("","*  * *  * *      **"),
+            String.join("","**** **** *    ****")
+        };
+        for (String line:lines){
+            System.out.println(line);
+        }
+
+    }
+}
